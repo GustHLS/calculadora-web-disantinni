e36f1a390494a104b4b5de57399066d858e5a750
gusthls <gustavohls_work@hotmail.com>
2025-09-08 21:57:27 -0300

    Adição de docstrings para normas da PEP 8

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 from flask import Flask, jsonify, render_template, request, session, redirect, url_for
 import uuid
 
-from scripts.home_data.home_data import lojas, anos, meses
+from scripts.home_data.home_data import get_lojas, get_meses, get_anos
 
 app = Flask(__name__)
 app.secret_key = 'Gustavo@2022Ds'
 
 @app.route('/')
 def home():
+    """
+    Rota principal.
+    Quando acessada, renderiza a página inicial com as listas de lojas,
+    anos e meses disponíveis.
+    """
     session.pop('grade_id', None)
     session['historico_funcionarios'] = 0
-    return render_template('index.html', lojas=lojas, anos=anos, meses=meses)
+
+    return render_template(
+        'index.html',
+        lojas=get_lojas(),
+        meses=get_meses(),
+        anos=get_anos()
+    )
