e9d1294b968289a8e5d4a8ca6e8c6f26eb9a36d9
gusthls <gustavohls_work@hotmail.com>
2025-09-08 21:56:05 -0300

    Adição de docstrings para seguir normas da PEP 8

--- scripts/home_data/home_data.py
+++ scripts/home_data/home_data.py
@@ -4,17 +4,44 @@ import pandas as pd
 
 from scripts.db_connection.db import engine
 
-# Query para pegar as lojas que estão ativas, não são franquias e são SMART.
-query = "select distinct Loja from loja where LOJA_ATIVA = 'SIM' and LOJA_MODELO = 'S' AND PROPRIA_FRANQUIA = 'Propria'"
-df = pd.read_sql(query, engine)
-lojas = df['Loja'].tolist()
 
-# Ano atual até 2018, são as possibilidades para rodar a calculadora.
-ano_atual = datetime.now().year + 1
-anos = [str(ano) for ano in range(ano_atual, 2017, -1)]
+def get_lojas():
+    """
+    Retorna a lista de lojas que são SMART, estão ativas e são próprias.
+    Returns:
+        list[str]: Lista de nomes de lojas.
+    """
 
-# Lista de todos os meses do ano.
-meses = [
-    'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
-    'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
-]
+    query = "select distinct Loja from loja where LOJA_ATIVA = 'SIM' and LOJA_MODELO = 'S' AND PROPRIA_FRANQUIA = 'Propria'"
+    df = pd.read_sql(query, engine)
+    lojas = df['Loja'].tolist()
+
+    return lojas
+
+
+def get_anos():
+    """
+    Retorna a lista com os meses do ano em português.
+    Returns:
+        list[str]: Lista de anos como strings.
+    """
+
+    ano_atual = datetime.now().year + 1
+    anos = [str(ano) for ano in range(ano_atual, 2017, -1)]
+
+    return anos
+
+
+def get_meses():
+    """
+    Função de retorno da lista de meses do ano.
+    Returns:
+        list[str]: Lista de nomes dos meses.
+    """
+
+    meses = [
+        'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
+        'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
+    ]
+
+    return meses
